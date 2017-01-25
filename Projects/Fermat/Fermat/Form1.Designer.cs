abec01c3bc393af65d7c40d77816427365019a7d
Melanie Lambson <melanieharini@gmail.com>
2017-01-24 22:40:17 -0700

    first commit

---
+++ Projects/Fermat/Fermat/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Fermat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.input = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.k = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(41, 37);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(199, 20);
+            this.input.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // k
+            // 
+            this.k.Location = new System.Drawing.Point(41, 63);
+            this.k.Name = "k";
+            this.k.Size = new System.Drawing.Size(199, 20);
+            this.k.TabIndex = 2;
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(41, 89);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(199, 20);
+            this.output.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "solve";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.k);
+            this.Controls.Add(this.input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox k;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Projects/Fermat/Fermat/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Fermat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+		 /*
+		  * Function that will call the Fermat test after click of the button "solve".
+		  * The time complexity of this algorithm is O(kval*log^3(n)).
+		  * Constraints: 1 <= k < (n - 1) since the random numbers generated must be greater than 2
+		  * and smaller than n (n - 2 possibilities).
+		  */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kval = Convert.ToInt32(k.Text);
+            int n = Convert.ToInt32(input.Text);
+
+            if(kval >= 1 && kval < (n - 1)) {
+                if (primality(n, kval))
+                {
+                    double p = calculateProb(kval);
+                    output.Text = "yes with probability " + p.ToString();
+                }
+                else
+                {
+                    output.Text = "no";
+                }
+            }
+            else
+            {
+                output.Text = "k value out of range";
+            }
+        }
+
+		/*
+		 * Function that will perform the Fermat's test with a time complexity of O(kval).
+		 * When random numbers are generated, they are compared to the set of numbers already
+		 * tested to make sure no number is used twice.
+		 * @param n: the integer to test for primality
+		 * @param kval: the number of random integers to test
+		 * return true if the test passed
+		 *        false otherwise
+		 */
+        private bool primality(int n, int kval)
+        {
+            HashSet<int> tested = new HashSet<int>();
+            Random random = new Random();
+            int randomNumber = random.Next(2, n);
+            while (tested.Count < kval)
+            {
+                if (!tested.Contains(randomNumber))
+                {
+                    if (modexp(randomNumber, n - 1, n) != 1) { return false; }
+                    tested.Add(randomNumber);
+                }
+                randomNumber = random.Next(2, n - 1);
+            }
+            return true;
+        }
+
+		/*
+		 * This function calculates x^y mod n with a complexity time of O(n^3)
+		 * @param x: random number generated in primality()
+		 * @param y: the exponent of x
+		 * @param n: the integer to test for primality
+		 * return x^y mod n
+		 */
+        private BigInteger modexp(int x, double y, int n)
+        {
+            if(y == 0) { return 1; }
+            BigInteger z = modexp(x, Math.Floor(y/2), n);
+            if(y % 2 == 0)
+            {
+                BigInteger temp = (z * z) % n;
+                return temp;
+            }
+            else
+            {
+                BigInteger temp = (x * (z * z)) % n;
+                return temp;
+            }
+        }
+
+		/*
+		 * This function calculates the probability that p is an actual prime number and not
+		 * a composite with a constant time complexity.
+		 * @param kval: the number of values tested
+		 * return 1 - p the probability of n being a prime number
+		 */
+        private double calculateProb(int kval)
+        {
+            double p = 1 / (Math.Pow(2, kval));
+            return 1 - p;
+        }
+        private bool isCarmicheal()
+        {
+            return false;
+        }
+    }
+}
